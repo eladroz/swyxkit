8e2a9c0b080d7c6afc28e33430a2ee0275cc3c98
Elad Rosenheim <elad.rosenheim@gmail.com>
2025-09-22 12:26:22 +0300

    Use fork's issues

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -1,6 +1,6 @@
-export const SITE_URL = 'https://swyxkit.netlify.app';
-export const APPROVED_POSTERS_GH_USERNAME = ['swyxio'];
-export const GH_USER_REPO = 'swyxio/swyxkit'; // used for pulling github issues and offering comments
+export const SITE_URL = 'https://eladroz-swyxkit.netlify.app';
+export const APPROVED_POSTERS_GH_USERNAME = ['swyxio', 'eladroz'];
+export const GH_USER_REPO = 'eladroz/swyxkit'; // used for pulling github issues and offering comments
 export const SITE_TITLE = 'SwyxKit';
 export const SITE_DESCRIPTION = "swyx's default SvelteKit + Tailwind starter";
 export const DEFAULT_OG_IMAGE =
